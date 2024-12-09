00e3fd2f3207c7e8a0e53317aa124164b6b88d09
Gunottam Maini <138522576+gunottam@users.noreply.github.com>
2024-12-09 06:43:45 +0530

    Update bst.c

--- bst.c
+++ bst.c
@@ -1,99 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-typedef struct node{
+typedef struct node {
+      struct node * l;
+      struct node * r;
       int data;
-      struct node *next;
-}node;
+      }node;
 
-typedef struct graph{
-      node * head;
-}graph;
-
-graph * create(int n){
-      graph * G = (graph *)malloc(n*sizeof(graph));
-      for(int i = 0 ; i < n ; i++)
-            G[i].head = NULL;
-
-      return G;
+void insert(node **, int);
+void inorder(node *);
+void del(node **,int);
+node * findnext(node * a){
+      while(a != NULL && a->r != NULL)
+            a = a->l;
+      return a;
 }
-
-void addedge(graph * G, int s, int d){
-      node * newnode = (node *)malloc(sizeof(node));
-      newnode->data = d;
-      newnode->next = G[s].head;
-      G[s].head = newnode;
-
-      newnode = (node *)malloc(sizeof(node));
-      newnode->data = s;
-      newnode->next = G[d].head;
-      G[d].head = newnode;
-}
-
-void printgraph(graph * G,int n){
-      node * current = NULL;
-      for(int i = 0 ; i < n ; i++){
-            current = G[i].head;
-            printf("Vertex %d: ",i);
-            while(current != NULL){
-                  printf("%d->",current->data);
-                  current = current->next;
-            }
-            printf("NULL\n");
-      }
-      }
-
-void bfs(graph *G, int start, int n) {
-    int f = 0 , r = 0;
-    int *visited = (int *)calloc(n , sizeof(int));
-    int *queue = (int *)calloc(n , sizeof(int));
-    visited[start] = 1;
-    queue[r++] = start;
-    while(f<r){
-      int curr = queue[f++];
-      printf("%d ",curr);
-      node * t = G[curr].head;
-      while(t){
-            if(!visited[t->data]){
-                  visited[t->data] = 1;
-                  queue[r++] = t->data;
-            }
-            t = t->next;
-      }
-    }
-}
-
-void dfs(graph *G, int start,int * vis) {
-    vis[start] = 1;
-    printf("%d -> ", start);
-
-    node *current = G[start].head;
-    while (current) {
-        if (!vis[current->data]) {
-            dfs(G, current->data,vis);
-        }
-        current = current->next;
-    }
-    
-}
-
 int main(){
-      int n,a,b;
-      int visited[100] = {0};
-      printf("Enter number of vertices: ");
-      scanf("%d",&n);
-
-      graph * G = create(n);
-
-      for(int i = 0 ; i < n ; i++){
-            printf("Add Edge: ");
-            scanf("%d%d",&a,&b);
-            addedge(G,a,b);
+      int c = 1;
+      node*root = NULL;
+      while (c!=-1)
+      {
+            printf("\n1. Insert\n2. Inorder\n3. Delete\n4. Exit\n");
+            scanf("%d",&c);
+            switch(c){
+                  case 1:
+                        int x;
+                        scanf("%d",&x);
+                        insert(&root, x);
+                        break;
+                  case 2:
+                        inorder(root);
+                        break;
+                  case 3:
+                        int y;
+                        scanf("%d",&y);
+                        del(&root,y);
+                        break;
+                  default:
+                        c=-1;
+                        break;
+            }
       }
+      
 
-      printgraph(G,n);
-      bfs(G,0,n);
-      dfs(G,0,visited);
       return 0;
 }
+
+void insert(node ** root , int d){
+      if(*root == NULL){
+            *root = (node*)malloc(sizeof(node));
+            (*root)->data = d;
+            (*root)->l = NULL;
+            (*root)->r = NULL;
+      }
+      else if(d < (*root)->data)
+            insert(&((*root)->l),d);
+      else if(d>(*root)->data)
+            insert(&((*root)->r),d);
+}
+
+void inorder(node * root){
+      if(root == NULL) return;
+
+      inorder(root->l);
+      printf("%d ",root->data);
+      inorder(root->r);
+}
+
+void del(node ** rootref, int key){
+      node * root = *rootref;
+      if(root == NULL)
+            return;
+      
+      else if(key < root->data)
+            del(&(root->l),key);
+      else if(key>root->data)
+            del(&(root->r),key);
+      else{
+            if(root->l == NULL && root->r == NULL){
+                  free(root);
+                  *rootref = NULL;
+            }
+            else if(root->l == NULL){
+                  node * temp = root->r;
+                  free(root);
+                  *rootref = temp;
+                  }
+            else if(root->r == NULL){
+                  node * temp = root->l;
+                  free(root);
+                  *rootref = temp;
+            }
+            else{
+                  node * temp = findnext(root->r);
+                  root->data = temp->data;
+                  del(&(root->r),temp->data);
+            }
+      }
+}
+      
